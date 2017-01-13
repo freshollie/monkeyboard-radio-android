d9457015781818d8a58856382dd090c26b40d5dc
Oliver Bell <freshollie@gmail.com>
2017-01-13 00:45:49 +0000

    Continued development of backend. Added more commands to radio

--- app/src/main/java/com/freshollie/monkeyboarddabradio/radio/RadioDevice.java
+++ app/src/main/java/com/freshollie/monkeyboarddabradio/radio/RadioDevice.java
@@ -14,24 +14,36 @@ public class RadioDevice {
     public static final int BAUD_RATE = 57600;
 
     private static class ByteValues {
-        public static byte CMD_ACK = 0x01;
-        public static byte START_BYTE = (byte) 0xFE;
-        public static byte END_BYTE = (byte) 0xFD;
+        static byte RESPONSE_TYPE_ACK = 0x00;
+        static byte CMD_ACK = 0x01;
+        static byte CMD_NAK = 0x02;
 
-        public static byte SERIAL_NUMBER = 0x01;
+        static byte START_BYTE = (byte) 0xFE;
+        static byte END_BYTE = (byte) 0xFD;
 
-        public static byte CATEGORY_STREAM = 0x01;
-        public static byte STREAM_Play = 0x00;
-        public static byte STREAM_Stop = 0x01;
-        public static byte STREAM_AutoSearch = 0x03;
-        public static byte STREAM_StopSearch = 0x04;
+        static byte SERIAL_NUMBER = 0x01;
 
-        public static byte STREAM_GetPlayStatus = 0x05;
+        static byte FUNCTION_TYPE_STREAM = 0x01;
+        static byte STREAM_Play = 0x00;
+        static byte STREAM_Stop = 0x01;
+        static byte STREAM_AutoSearch = 0x03;
+        static byte STREAM_StopSearch = 0x04;
+        static byte STREAM_GetPlayStatus = 0x05;
+        static byte STREAM_GetPlayIndex = 0x07;
+        static byte STREAM_SetStereoMode = 0x09;
+        static byte STREAM_GetStereo = 0x0B;
+        static byte STREAM_SetVolume = 0x0C;
+        static byte STREAM_GetVolume = 0x0D;
+        static byte STREAM_GetProgramType = 0X0E;
+        static byte STREAM_GetProgramName = 0x0F;
+        static byte STREAM_GetProgramText = 0x10;
+        static byte STREAM_GetDataRate = 0x12;
+        static byte STREAM_GetSignalQuality = 0x13;
+        static byte STREAM_GetEnsembleName = 0x15; // Name of the DAB collection
+        static byte STREAM_GetTotalProgram = 0x16;
 
 
-        public static byte STREAM_SetVolume = 0x0C;
-
-        public static byte STREAM_MODE_DAB = 0x00;
+        static byte STREAM_MODE_DAB = 0x00;
 
         public static
     }
@@ -108,15 +120,15 @@ public class RadioDevice {
     /**
      * Calls the given function and returns the values using the serial connection
      *
-     * @param functionCategory
+     * @param functionType
      * @param function
      * @param parameters
      * @return
      */
-    private byte[] call(byte functionCategory, byte function, byte[] parameters) {
+    private byte[] call(byte functionType, byte function, byte[] parameters) {
         byte [] buffer = new byte[64];
         buffer[0] = ByteValues.START_BYTE;
-        buffer[1] = functionCategory;
+        buffer[1] = functionType;
         buffer[2] = function;
         buffer[3] = ByteValues.SERIAL_NUMBER;
         buffer[4] = 0x00;
@@ -131,20 +143,40 @@ public class RadioDevice {
 
         buffer[i + 1] = ByteValues.END_BYTE;
 
-        return connection.sendForResponse(buffer);
+        byte[] response = connection.sendForResponse(buffer);
+
+        if (isResponse(response) &&
+                (isCommandAck(response) || isCorrectResponse(response, functionType, function))) {
+            return response;
+        } else {
+            return null;
+        }
     }
 
     public boolean setVolume(int volume) {
-        return isCommandAck(
-                call(
-                        ByteValues.CATEGORY_STREAM,
-                        ByteValues.STREAM_SetVolume,
-                        new byte[]{
-                                (byte) volume
-                        }
+        return call(
 
-                )
-        );
+                ByteValues.FUNCTION_TYPE_STREAM,
+                ByteValues.STREAM_SetVolume,
+                new byte[]{
+                        (byte) volume
+                }
+        ) != null;
+    }
+
+    public int getVolume() {
+        byte[] response =
+                call(
+                        ByteValues.FUNCTION_TYPE_STREAM,
+                        ByteValues.STREAM_GetVolume,
+                        new byte[]{}
+                );
+
+        if (response != null) {
+            return response[6];
+        } else {
+            return -1;
+        }
     }
 
     public boolean setChannel(int channelNum) {
@@ -152,72 +184,117 @@ public class RadioDevice {
     }
 
     public boolean play(int channelNum) {
-        return isCommandAck(
-                call(
-                        ByteValues.CATEGORY_STREAM,
-                        ByteValues.STREAM_Play,
-                        new byte[]{
-                                ByteValues.STREAM_MODE_DAB,
-                                0x00, 0x00, 0x00, (byte) channelNum, // 0x000000NN
-                        }
-                )
-        );
+        return call(
+                ByteValues.FUNCTION_TYPE_STREAM,
+                ByteValues.STREAM_Play,
+                new byte[]{
+                        ByteValues.STREAM_MODE_DAB,
+                        0x00, 0x00, 0x00, (byte) channelNum, // 0x000000NN
+                }
+        ) != null;
     }
 
     public boolean stop() {
-        return isCommandAck(
-                call(
-                        ByteValues.CATEGORY_STREAM,
-                        ByteValues.STREAM_Stop,
-                        new byte[]{}
-                )
-        );
+        return call(
+                ByteValues.FUNCTION_TYPE_STREAM,
+                ByteValues.STREAM_Stop,
+                new byte[]{}
+        ) != null;
     }
 
     public boolean channelScan() {
-        return isCommandAck(
-                call(
-                        ByteValues.CATEGORY_STREAM,
-                        ByteValues.STREAM_AutoSearch,
-                        new byte[]{}
-                )
-        );
+        return call(
+                ByteValues.FUNCTION_TYPE_STREAM,
+                ByteValues.STREAM_AutoSearch,
+                new byte[]{}
+        ) != null;
     }
 
     public boolean stopScan() {
-        return isCommandAck(
-                call(
-                        ByteValues.CATEGORY_STREAM,
-                        ByteValues.STREAM_AutoSearch,
-                        new byte[]{}
-                )
-        );
+        return call(
+                ByteValues.FUNCTION_TYPE_STREAM,
+                ByteValues.STREAM_AutoSearch,
+                new byte[]{}
+        ) != null;
     }
 
     public int getPlayStatus() {
         byte[] response =
                 call(
-                        ByteValues.CATEGORY_STREAM,
+                        ByteValues.FUNCTION_TYPE_STREAM,
                         ByteValues.STREAM_GetPlayStatus,
                         new byte[]{}
                 );
 
-        if (isResponse(response) && isCommandType(response, ByteValues.CATEGORY_STREAM)) {
+        if (response != null) {
             return response[6];
         } else {
             return -1;
         }
     }
 
-    private boolean isCommandType(byte[] response, byte type) {
-        return response[1] == type;
+    public int getPlayIndex() {
+        byte[] response =
+                call(
+                        ByteValues.FUNCTION_TYPE_STREAM,
+                        ByteValues.STREAM_GetPlayIndex,
+                        new byte[]{}
+                );
+
+        if (response != null) {
+            return response[9];
+        } else {
+            return -1;
+        }
+    }
+
+    public boolean setStereoMode(int mode) {
+        return call(
+                ByteValues.FUNCTION_TYPE_STREAM,
+                ByteValues.STREAM_SetStereoMode,
+                new byte[]{
+                        (byte) mode
+                }
+        ) != null;
+    }
+
+    public int getStereo() {
+        byte[] response =
+                call(
+                        ByteValues.FUNCTION_TYPE_STREAM,
+                        ByteValues.STREAM_GetStereo,
+                        new byte[]{}
+                );
+
+        if (response != null) {
+            return response[9];
+        } else {
+            return -1;
+        }
+    }
+
+    public int getProgramType() {
+
+    }
+
+    private boolean isCorrectResponse(byte[] response, byte type, byte command) {
+        return response[1] == type && response[2] == command;
     }
     private boolean isResponse(byte[] response) {
         return response[0] == ByteValues.START_BYTE;
     }
 
+    /**
+     * Checks if a response byte array is acknowledging completion of a command
+     *
+     * @param response buffer
+     * @return
+     */
     private boolean isCommandAck(byte[] response) {
-        return isResponse(response) && response[2] == ByteValues.CMD_ACK;
+        return isResponse(response) &&
+                response[1] == ByteValues.RESPONSE_TYPE_ACK &&
+                response[2] == ByteValues.CMD_ACK &&
+                response[5] == 0;
     }
 
     /**
